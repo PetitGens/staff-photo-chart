81cf15a2451be78d6a762537a73bd80690674a48
moustave <94120536+Moustave@users.noreply.github.com>
2024-03-07 17:47:07 +0100

    Created DatabaseConfigWindow 's view

--- PhotoChartApp/DatabaseConfigWindow.xaml.cs
+++ PhotoChartApp/DatabaseConfigWindow.xaml.cs
@@ -10,21 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PhotoChartApp
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour DatabaseConfigWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DatabaseConfigWindow : Window
     {
-        public MainWindow()
+        public DatabaseConfigWindow()
         {
             InitializeComponent();
-            DatabaseConfigWindow d = new DatabaseConfigWindow();
-            d.ShowDialog();
+
+            
         }
     }
 }
